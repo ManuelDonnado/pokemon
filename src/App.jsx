c9df10cbdeb79138f0bebef5bb3be4249a15a85f
Manuel <manueldonadoh@gmail.com>
2023-09-19 20:30:22 -0600

    edicion pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,42 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Nav from './components/Nav';
+import Card from './components/Card';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pokemon, setPokemon] = useState ("pikachu");
+  const [data, setData] = useState (null) ; 
+
+  useEffect (() => {
+    const getData = async () => {
+      const res = await fetch (`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+      const datos = await res.json();
+
+      setData(datos);
+    }
+    getData();
+  }, [pokemon]) ;
+
+  const handleSubmit = (e) => {
+  e.preventDefault();
+  const pokeName = e.target[0].value;
+  setPokemon(pokeName);
+  }
+
+  const handleClick = (e) => {
+    const pokeName = e.target.value;
+    setPokemon(pokeName)
+  }
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <Nav  handleSubmit= {handleSubmit} handleClick = {handleClick} />
+      {data && <Card data={data}/>}
     </>
   )
 }
